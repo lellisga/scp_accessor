c34c7ed0b04b19a58ca154b32d31e92b57f3b486
Li Ellis Gallardo <lellisga@gmail.com>
2012-02-17 16:29:25 -0500

    add some test case to verify the existence and integrity of the configuration file

--- lib/scpAccessor/application.rb
+++ lib/scpAccessor/application.rb
@@ -2,31 +2,57 @@ require 'yaml'
 
 module ScpAccessor
   class Application
-
-    attr_accessor :configuration
+    DefaultConfigurationFileLocation = "#{File.dirname(__FILE__)}/../../Conf/configuration.yaml"
     
-    def initialize(output)
+    def initialize(output, configuration_file_location = DefaultConfigurationFileLocation)
       @output = output
-      read_configuration_file
+      @configuration = read_configuration_file(configuration_file_location)
     end
     
     def start
       @output.puts 'Welcome to SCP Accessor!'
+#      print_configuration_file
     end
 
     private
-    
-    def read_configuration_file
-      begin
-        @configuration = YAML.load_file("#{File.dirname(__FILE__)}/../../Conf/configuration.yaml")
-        @output.puts "Configuration file was loaded..."
-      rescue Errno::ENOENT
-        @output.puts "SpcAccessor wasn't able to find the file located in the Config folder"
-      end
-      
 
+    def print_configuration_file
+      @configuration.each_pair {|key, value|
+        if value["active"] == true
+          @output.puts "#{key} : #{value}"
+        end
+      }
+    end
+
+    def verify_file_content(configuration)
+      missing = []
+      configuration.each_pair {|key, value|
+
+        next if value["active"] != nil and value["active"] == false
+
+        missing << "active" if value["active"] == nil
+        missing << "server" if value["server"] == nil
+        missing << "user" if value["user"] == nil
+        missing << "file_1" if value["file_1"] == nil
+        missing << "out_dir" if value["out_dir"] == nil
+        break if missing.size > 0
+      }
+      raise ("The following fields are mandatory: #{missing.join(',')}") if missing.size > 0
+    end
+    
+    def read_configuration_file(configuration_file_location)
+      #begin
+      configuration = YAML.load_file(configuration_file_location)
+      verify_file_content(configuration)
+      @output.puts "Configuration file was loaded..."
+      #rescue Errno::ENOENT => e
+      #@output.puts "Spc Accessor wasn't able to find the file located in the Config folder."
+      #abort
+      #end
       
+      configuration
     end
     
   end
+
 end
--- spec/scpAccessor/scp_accessor_spec.rb
+++ spec/scpAccessor/scp_accessor_spec.rb
@@ -4,16 +4,36 @@ module ScpAccessor
   describe Application do
 
     let(:output){double('output').as_null_object}
-    let(:application){Application.new(output)}
+    let(:application){Application.new(output,"#{File.dirname(__FILE__)}/../../Conf/configuration_example.yaml")}
     
     context "Loading the YAML file" do
-      it "should load for the YALM file" do
-        output.should_not_receive(:puts).with(/SpcAccessor wasn't able to find the file/)
-        output.should_receive(:puts).with(/Configuration file was loaded.../)
-        application.start
+
+      describe "#read_configuration_file" do
+        it "should read the YALM file without no problems" do
+          output.should_not_receive(:puts).with(/SpcAccessor wasn't able to find the file/)
+          output.should_receive(:puts).with(/Configuration file was loaded.../)
+          application.start
+        end
+        
+        it "should raise an error because the file doesn't exists" do
+          lambda{Application.new(output,"#{File.dirname(__FILE__)}/../../Conf/configuration_example_dont_Exist.yaml")}.should \
+          raise_error(Errno::ENOENT)
+        end
       end
       
-      it "should print the configuration"
+      context "Verifying the content of the file" do
+        describe "#verify_file_content" do
+          it "should load the configuration file without no problem" do
+            output.should_receive(:puts).with(/Configuration file was loaded.../)
+            application.start
+          end
+          
+          it "should raise a MissingField Error when a field is missing" do
+            lambda{ Application.new(output,"#{File.dirname(__FILE__)}/../../Conf/configuration_example_2.yaml")}.should \
+            raise_error(/The following fields are mandatory:/)
+          end
+        end
+      end
     end
     
     context "Starting the application" do
